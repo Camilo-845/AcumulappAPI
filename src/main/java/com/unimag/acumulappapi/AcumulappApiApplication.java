60eb9fdc44e6ecd3f6b61b476d935171a1542eec
CarlosD25 <sanchezpalominocarlosdaniel@gmail.com>
2025-06-13 21:23:41 -0500

    configure local PostgreSQL database connection

---
+++ src/main/java/com/unimag/acumulappapi/AcumulappApiApplication.java
@@ -0,0 +1,13 @@
+package com.unimag.acumulappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcumulappApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcumulappApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unimag/acumulappapi/AcumulappApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.unimag.acumulappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class AcumulappApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
